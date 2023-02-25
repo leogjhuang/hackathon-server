66ce1bf34bd4752f257c22de5a7fbd1f89462a21
Leo Huang <leogjhuang@gmail.com>
2023-02-24 21:49:04 -0700

    Add update user endpoint

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from app import app
-from app.models import User
+from app.database import db
+from app.models import User, Skill
 
 
 @app.route('/users', methods=['GET'])
@@ -20,4 +21,34 @@ def get_user(user_id):
     user = User.query.get(user_id)
     if user is None:
         return jsonify({'error': 'User not found'}), 404
+    return jsonify(user.serialize())
+
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id, name, company, email, phone, skills):
+    """
+    Update information about a specific user
+    """
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+    # Update properties if they are not None
+    if name is not None:
+        user.name = name
+    if company is not None:
+        user.company = company
+    if email is not None:
+        user.email = email
+    if phone is not None:
+        user.phone = phone
+    if skills is not None:
+        # Add new skills to the database and update rating
+        for skill in skills:
+            skill = Skill.query.filter_by(skill=skill['skill']).first()
+            if skill is None:
+                skill = Skill(**skill)
+                db.session.add(skill)
+            user.skills.append(skill)
+    # Commit the changes
+    db.session.commit()
     return jsonify(user.serialize())
