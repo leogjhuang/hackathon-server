533759f0cd2222f51c749c31136a3aac5f5d5ed3
Leo Huang <leogjhuang@gmail.com>
2023-02-24 20:29:05 -0700

    Fix user routes

--- app/routes/users.py
+++ app/routes/users.py
@@ -9,7 +9,7 @@ def get_users():
     Get a list of all users
     """
     users = User.query.all()
-    return jsonify([user.serialize for user in users])
+    return jsonify([user.serialize() for user in users])
 
 
 @app.route('/users/<int:user_id>', methods=['GET'])
@@ -20,4 +20,4 @@ def get_user(user_id):
     user = User.query.get(user_id)
     if user is None:
         return jsonify({'error': 'User not found'}), 404
-    return jsonify(user.serialize)
+    return jsonify(user.serialize())
